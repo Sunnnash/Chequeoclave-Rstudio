56f7206ac202cb1d1b964b156dd8adbeb0cd2725
Sunny <delarosa.barajas97@gmail.com>
2021-06-29 12:47:40 -0500

    Se aprendió a usar comentarios sin modificar el código

---
+++ Ejercicio 1.R
@@ -0,0 +1,22 @@
+sumando <- 1323455 + 67849
+sumando
+sumando.2 <- 890765 + 765679
+
+restando <- 45758 - 125468
+
+restando.2 <- 12589 - 168786
+
+multiplicando <- 287852 * 578965
+
+multiplicando.2 <- 45876 * 58321
+
+dividiendo <- 456/ 78
+
+dividiendo.2 <- 2779/ 122
+
+potencia <- 85^5
+
+potencia.2 <- 456^87
+q() 
+# <- este es el bueno 
+
